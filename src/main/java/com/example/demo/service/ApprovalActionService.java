8df2114e435dcf8d183a4b5d6deda00d0fcd9a3d
bala-amypo <bala@amypo.in>
2025-12-24 06:20:29 +0000

    User commit

--- src/main/java/com/example/demo/service/ApprovalActionService.java
+++ src/main/java/com/example/demo/service/ApprovalActionService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.ApprovalAction;
+import com.example.demo.entity.ApprovalRequest;
 import com.example.demo.repository.ApprovalActionRepository;
 import com.example.demo.repository.ApprovalRequestRepository;
 
@@ -14,15 +15,19 @@ public class ApprovalActionService {
     private final ApprovalActionRepository actionRepository;
     private final ApprovalRequestRepository requestRepository;
 
-    // Constructor order DOES NOT use security
     public ApprovalActionService(ApprovalActionRepository actionRepository,
                                  ApprovalRequestRepository requestRepository) {
         this.actionRepository = actionRepository;
         this.requestRepository = requestRepository;
     }
 
-    public ApprovalAction recordAction(ApprovalAction action) {
-        action.setActionDate(LocalDateTime.now());
+    public ApprovalAction takeAction(Long requestId, ApprovalAction action) {
+        ApprovalRequest request = requestRepository.findById(requestId)
+                .orElseThrow(() -> new RuntimeException("Request not found"));
+
+        action.setApprovalRequest(request);
+        action.setActionAt(LocalDateTime.now());
+
         return actionRepository.save(action);
     }
 }

c89000cd05b612dd3c17f72de99fff830c8ff225
bala-amypo <bala@amypo.in>
2025-12-26 06:27:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);  // Must have this!
-    boolean existsByUsername(String username);  // For checking duplicates
-    boolean existsByEmail(String email);        // For checking duplicates
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }

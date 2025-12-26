191d25e184cdb96e854e81e88da87d1de2ac28fb
bala-amypo <bala@amypo.in>
2025-12-26 05:18:14 +0000

    User commit

--- src/main/java/com/example/demo/service/ApprovalRequestService.java
+++ src/main/java/com/example/demo/service/ApprovalRequestService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.ApprovalRequest;
-
 import java.util.List;
 
 public interface ApprovalRequestService {
--- src/main/java/com/example/demo/service/WorkflowStepConfigService.java
+++ src/main/java/com/example/demo/service/WorkflowStepConfigService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.WorkflowStepConfig;
-
 import java.util.List;
 
 public interface WorkflowStepConfigService {

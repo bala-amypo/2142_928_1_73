1c36f93132758b95acdcc211ac88d6806d96b5fb
bala-amypo <bala@amypo.in>
2025-12-27 04:58:44 +0000

    User commit

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 

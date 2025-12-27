057cf55a164463fe1e7255414b746f4318e15abc
bala-amypo <bala@amypo.in>
2025-12-27 04:49:45 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +14,17 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Workflow API")
+                        .description("Spring Boot JWT Authentication API")
+                        .version("1.0.0"))
                 .servers(List.of(
-                        new Server().url("https://9156.408procr.amypo.ai/")
+                        new Server()
+                                .url("https://9156.408procr.amypo.ai")
+                                .description("Production Server"),
+                        new Server()
+                                .url("http://localhost:9001")
+                                .description("Local Development Server")
                 ));
     }
 }
